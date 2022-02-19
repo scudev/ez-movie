297a02b4b596e552ef54906b762704612726bdf7
hx-w <herixth@163.com>
2022-02-19 22:41:14 +0800

    init redis

--- server/backend/src/redis.py
+++ server/backend/src/redis.py
@@ -3,14 +3,8 @@
 import redis
 from .model import *
 
-redis_inst: redis.Redis = None
-
-def init_redis_inst():
-    global redis_inst
-    redis_inst = redis.Redis(
-        host='ezm-redis',
-        port=6379,
-        decode_responses=True
-    )
-
-init_redis_inst()
+redis_inst = redis.Redis(
+    host='ezm-redis',
+    port=6379,
+    decode_responses=True
+)
