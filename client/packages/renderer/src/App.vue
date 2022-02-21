9571ba617ad579cd0c3be15f1247376e9ad38cd4
hx-w <herixth@163.com>
2022-02-21 08:23:57 +0800

    clear home page

--- client/packages/renderer/src/App.vue
+++ client/packages/renderer/src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <Home />
   </div>
 </template>
 
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 </script>
 
 <style>
