b5daed4565f302051c2842e922e4b7ebf37002cd
hx-w <herixth@163.com>
2022-02-23 10:13:15 +0800

    some api

--- server/backend/server.py
+++ server/backend/server.py
@@ -5,6 +5,18 @@ from fastapi import FastAPI
 api = FastAPI()
 
 
-@api.get("/")
+@api.get('/')
 async def root():
     return {"message": "Hello World"}
+
+@api.get('video/detail')
+async def videoDetail(pwd: str):
+    return {'message': 'hello world'}
+
+@api.get('video/list')
+async def videoList(pwd: str):
+    return {'message': 'hello world'}
+
+@api.post('video/status')
+async def videoStatus():
+    pass
