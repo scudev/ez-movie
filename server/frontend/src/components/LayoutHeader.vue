bb63e427579a3881cedd711350aee2f878f74bb1
hx-w <herixth@163.com>
2022-03-07 09:35:38 +0800

    basic status change in server-frontend

---
+++ server/frontend/src/components/LayoutHeader.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="header fr ac jc">
+    {{projectName}}
+    <div class="route-icon">
+      <a-icon :type="iconData.icon" class="icon" @click="goRoute(iconData.route)" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      projectName: "爱看电影 服务端",
+      iconDataMap: {
+        '/': {
+          icon: 'unordered-list',
+          route: '/explore'
+        },
+        '/explore': {
+          icon: 'rollback',
+          route: '/'
+        }
+      },
+      iconData: {
+        icon: 'download',
+        route: '/explore'
+      }
+    }
+  },
+  components: {},
+  computed: {},
+  watch: {
+    '$route.path': {
+      handler (val) {
+        this.iconData = this.iconDataMap[val]
+      },
+      immediate: true
+    }
+  },
+  mounted () {},
+  created () {},
+  methods: {
+    goRoute (route) {
+      this.$router.push(route)
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.header{
+  position: relative;
+  box-sizing: border-box;
+  height: 28px;
+  -webkit-app-region: drag;
+  color: @primary-color;
+  font-weight: bold;
+  font-size: 32px;
+  .route-icon{
+    position: absolute;
+    bottom: -53px;
+    left: 64px;
+    z-index: 99;
+    cursor: pointer;
+    .icon{
+      font-size: 28px;
+      color: @primary-color;
+    }
+  }
+}
+</style>
--- server/frontend/src/main.js
+++ server/frontend/src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
+import antd from 'ant-design-vue'
+import VueCookies from 'vue-cookies'
 import App from './App.vue'
 import router from './router'
-import antd from 'ant-design-vue'
 import api from './utils/api'
 import 'ant-design-vue/dist/antd.less'
 import './assets/style/main.less'
 
 Vue.use(antd)
+Vue.use(VueCookies)
 
 Vue.prototype.$http = api
 Vue.config.productionTip = false
